095fd89c9d0b3dfe493c44e360d2084cf5ded9ab
x-dew <69053445+x-dew@users.noreply.github.com>
2021-04-28 21:57:33 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
+import Header from "./components/header";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className='container'>
+                <Header/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
