61c64919373f91e427f121d8f3eef87e3fb3f545
x-dew <69053445+x-dew@users.noreply.github.com>
2021-04-29 13:16:29 +0300

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import './../style/header.css'
+import {CustomInput, Form, FormGroup, Label} from 'reactstrap';
 
-const Header =(props)=>{
-return(
-    <div className='header'>
 
-    </div>
-)
+const Header = (props) => {
+    return (
+        <div className='header flex'>
+            <div className='header-menu flex'>
+                <h1>Все проекты</h1>
+                <div>
+                    <ul className='flex header-list'>
+                        <li className='active-list'>Все</li>
+                        <li>Займы</li>
+                        <li>Доли</li>
+                        <li>Акции</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <Form>
+                    <FormGroup className='form-switch flex'>
+                        <p>Только доступные к инвестированию </p>
+                            <CustomInput type="switch" id="exampleCustomSwitch" name="customSwitch"  />
+                    </FormGroup>
+                </Form>
+            </div>
+        </div>
+    )
 
 
 }
